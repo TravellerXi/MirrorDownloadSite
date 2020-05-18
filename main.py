9165ce83fe09c76873608e35c5f80154de8168f0
Traveller <51471579+TravellerXi@users.noreply.github.com>
2020-05-18 16:42:14 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,25 +1,16 @@
 #!/usr/bin/python3
+
 # coding:utf-8
-#Author: Github https://github.com/TravellerXi/
-#Need to build up a file tree like this:
-
-#├── Download_Programm_SourceCode
-#├── README.html
-#├── thirdparty
-#│   ├── http
-#│   ├── https
-#│   └── README.html
-
-#this py file need to put at Download_Programm_SourceCode folder.
-
-from flask import Flask,request,session,redirect,Response
+#import shutil
+from flask import Flask,request
+import _thread
 import os
-import time
+
 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-downloadSite='https://mirror.fastspeedgo.xyz' ##enter the apache ftp site that you have built
+downloadSite='https://mirror.fastspeedgo.xyz'
 
 
 
@@ -65,10 +56,10 @@ console.log("Github https://github.com/TravellerXi/")
  <title>Request Download</title>
  </head>
 <body>
-<p>Pls paste the download link to 'Download URL'. Once you click 'Request Download to our mirror', our server will download the resource automatically and deliver a mirror URL with fast download speed to you.
+<p>Pls paste the download link to 'Download URL'. Once you have clicked 'Request Download to our mirror', our server will download the resource automatically and deliver a mirror URL with fast download speed to you.
 <br>
 <p>请在下方'Download URL'处填入下载地址，然后点击'Request Download to our mirror',我们服务器将在后台下载,并提供一个速度快到飞起的链接给您。
-<p>返回镜像主站点，<a href='https://mirror.fastspeedgo.xyz/' target='_blank'>请点此</a>。
+<p>返回镜像主站点，Back to main mirror <a href='https://mirror.fastspeedgo.xyz/' target='_blank'>请点此 Click Here</a>。
 
 <form action="/" method="post">
                     <input type="hidden" style="visibility: hidden;" name="formname" value="usernamepasswd" />
@@ -123,8 +114,8 @@ console.log("Github https://github.com/TravellerXi/")
  <title>Download source</title>
  </head>
 <body>
-<h1> Download URL has been generated, pls click below link to go to download page. <br>
-<h1>镜像链接已生成，点击下方'download'前往下载地址<br>
+<h1> System now is downloading your resource. Download URL has been generated, pls click below link to go to download page to check if you can download now. <br>
+<h1>系统正在下载您的资源，镜像链接已生成，点击下方'download'前往下载地址以查看是否可以下载。<br>
 
 <td><a href="
 
@@ -179,13 +170,19 @@ console.log("Github https://github.com/TravellerXi/")
 downloadUrl_after='''
 
 " target="_blank">download</a></td><br>
-<p><a href='https://requestmirror.fastspeedgo.xyz/' target='_blank'>再请求一次下载</a>
+<p><a href='https://requestmirror.fastspeedgo.xyz/' target='_blank'>再请求一次下载 Download another resource</a>
 
 </body>
 </html>
 
 '''
 
+def Mkdir_andDownload(RealPath,url):
+    os.system('mkdir -p ' + RealPath)
+    os.system("touch "+RealPath+"/README.html")
+    os.system('echo "please do not download this file as server is still downloading. Once you have refreshed the page and you can not see this notice, then you can download all the file." > '+RealPath+"/README.html")
+    os.system('wget -P ' + RealPath + ' ' + url)
+    os.system('rm -rf '+RealPath+"/README.html")
 
 
 
@@ -204,9 +201,11 @@ def download_post():
         if os.path.exists(RealPath):
             return (downloadUrl_before_type2+download_url+downloadUrl_after)
         else:
-            os.system('mkdir -p '+RealPath)
-            os.system('wget -P '+RealPath+' '+ url)
-            return (downloadUrl_before + download_url+downloadUrl_after)
+            try :
+                _thread.start_new_thread(Mkdir_andDownload,(RealPath,url))
+                return (downloadUrl_before + download_url+downloadUrl_after)
+            except:
+                return ('System internal error, please try again<br><a href="https://requestmirror.fastspeedgo.xyz">back to index</a>')
     elif url.find('http://')>-1:
         path = url[7:]
         RealPath = os.getcwd()[:os.getcwd().find('Download_Programm_SourceCode')] + '/thirdparty/http/' + path
@@ -214,11 +213,11 @@ def download_post():
         if os.path.exists(RealPath):
             return (downloadUrl_before_type2 + download_url+downloadUrl_after)
         else:
-            os.system('mkdir -p ' + RealPath)
-            os.system('wget -P '+RealPath+' '+ url)
-            return (downloadUrl_before + download_url+downloadUrl_after)
-
-
+            try :
+                _thread.start_new_thread(Mkdir_andDownload,(RealPath,url))
+                return (downloadUrl_before + download_url+downloadUrl_after)
+            except:
+                return ('System internal error, please try again<br><a href="https://requestmirror.fastspeedgo.xyz">back to index</a>')
 
 
 
